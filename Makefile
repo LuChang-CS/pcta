1c5fef328634b4a5de27482ccb3044f62aacdadb
luchang <luchang@lab108.fudan.edu.cn>
2018-02-01 17:45:20 +0800

    add Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+main:
+	g++ -g -Wall -O2 -std=c++17 pcta/main.cpp -o target/main -lstdc++fs
--- pcta/main.cpp
+++ pcta/main.cpp
@@ -8,8 +8,8 @@ namespace fs = std::experimental::filesystem;
 using namespace std;
 
 int main() {
-    fs::path p = "C:/Users/chang/Documents/luchang/workspace/www/demo/test.json";
-    cout << p.string();
+    fs::path p = "/media/sdb1/luchang/workspace/fdu/dataset/test/categories_dict";
+    cout << p.string() << endl;
     string content = utils::fileInputContent(p);
     cout << content << endl;
     return 0;
