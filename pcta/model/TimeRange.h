faa6d84220da1b5c50d2487052e2da996d683f5f
LUCHANG_CS <2389156163@qq.com>
2018-02-01 16:06:56 +0800

    add time range class

---
+++ pcta/model/TimeRange.h
@@ -0,0 +1,39 @@
+#include <string>
+#include <ctime>
+
+#include "../utils.h"
+
+class TimeRange {
+    time_t start;
+    time_t end;
+    int timeSlotNumber;
+
+private:
+    void parse(const std::string &start, const std::string &end) {
+        this->start = utils::str2time(start);
+        this->end = utils::str2time(end);
+    }
+
+public:
+    TimeRange(const std::string &start, const std::string &end) {
+        this->parse(start, end);
+        this->timeSlotNumber = utils::daysbetween(this->start, this->end);
+    }
+
+    int getTimeSlotNumber() {
+        return this->timeSlotNumber;
+    }
+
+    int getTimeSlotNumber(const std::string &date) {
+        time_t t = utils::str2time(date);
+        return utils::daysbetween(this->start, t);
+    }
+
+    time_t getStart() {
+        return this->start;
+    }
+
+    time_t getEnd() {
+        return this->end;
+    }
+};
