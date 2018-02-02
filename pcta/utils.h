945f783901975e9254606cbb386f4232bc0c6efb
luchang <luchang@lab108.fudan.edu.cn>
2018-02-02 18:01:54 +0800

    add some time and path functions

--- pcta/utils.h
+++ pcta/utils.h
@@ -22,11 +22,24 @@ namespace utils {
         return (int) ((end - start) / (24 * 60 * 60));
     }
 
+    time_t nextDate(time_t date) {
+        return date + 24 * 60 * 60;
+    }
+
     std::string fileInputContent(const fs::path &p) {
         std::ifstream in(p.string());
         std::stringstream buffer;
         buffer << in.rdbuf();
         return buffer.str();
+    }
+
+    fs::path getPathFromDate(time_t date) {
+        std::tm tm = *std::localtime(&date);
+        int year = tm.tm_year + 1900;
+        int month = tm.tm_mon + 1;
+        int day = tm.tm_mday;
+        fs::path p = std::to_string(year);
+        return p.append(std::to_string(month)).append(std::to_string(day));
     }
 }
 
