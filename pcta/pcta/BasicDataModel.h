e0621124fada74438b8f8fb8787bc74c447666aa
luchang <luchang@lab108.fudan.edu.cn>
2018-02-02 18:46:55 +0800

    add basic data model

---
+++ pcta/pcta/BasicDataModel.h
@@ -0,0 +1,104 @@
+#ifndef __BASIC_DATA_MODEL_H__
+#define __BASIC_DATA_MODEL_H__
+
+#include <iostream>
+#include <experimental/filesystem>
+
+#include "../libs/json/document.h"
+
+#include "DataModel.h"
+#include "../model/WordCountList.h"
+
+namespace fs = std::experimental::filesystem;
+namespace json = rapidjson;
+
+class BasicDataModel: public DataModel {
+
+private:
+    WordCountList ***wordCountLists;
+
+public:
+    BasicDataModel(Dictionary *dictionary, TimeRange *timeRange, CategoryRange *categoryRange) {
+        this->wordCountLists = new (WordCountList ***)[this->timeSlotNumber];
+        for (int m = 0; m < this->timeSlotNumber; ++m) {
+            WordCountList **list = new (WordCountList **)[this->categoryNumber];
+            for (int n = 0; n < this->categoryNumber; ++n) {
+                WordCountList *l = new WordCountList();
+                list[n] = l;
+            }
+            this->wordCountLists[m] = list;
+        }
+    }
+
+    ~BasicDataModel() {
+        for (int m = 0; m < this->timeSlotNumber; ++m) {
+            WordCountList **list = this->wordCountLists[m];
+            if (list != NULL) {
+                for (int n = 0; n < this->categoryNumber; ++n) {
+                    WordCountList *l = list[n];
+                    if (l != NULL) delete l;
+                }
+                delete list;
+            }
+        }
+        delete this->wordCountLists;
+    }
+
+    WordCountList *getWordCountList(int m, int n) {
+        if (m < 0 || n < 0 || m >= this->timeSlotNumber || n >= this->categoryNumber) {
+            return NULL;
+        }
+        return this->wordCountLists[m][n];
+    }
+
+    std::vector<WordCountList *>
+    getListsByRange(int timeStart, int timeEnd, int categoryStart, int categoryEnd) {
+        std::vector<WordCountList *> result;
+        result.reserve((timeEnd - timeStart) * (categoryEnd - categoryStart));
+        for (int m = timeStart; m < timeEnd; ++m) {
+            WordCountList **list = this->wordCountLists[m];
+            for (int n = categoryStart; n < categoryEnd; ++n) {
+                result.push_back(list[n]);
+            }
+        }
+        return result;
+    }
+
+    void build(const fs::path &basePath) {
+        std::cout << "building basic model" << std::endl;
+        time_t current = this->timeRange->getStart();
+        time_t end = this->timeRange->getEnd();
+        while (current < end) {
+            int timeSlotIndex = this->timeRange->getTimeSlotNumber(current);
+            std::cout << "building " << timeSlotIndex << " in " << this->timeSlotNumber << std::endl;
+
+            fs::path p = basePath / utils::getPathFromDate(current) / "count";
+            std::string content = utils::fileInputContent(p);
+            if (content.length() == 0) continue;
+
+            json::Document d;
+            d.Parse(content.c_str());
+            json::Value::ConstMemberIterator it = d.MemberBegin();
+            while (it != d.MemberEnd()) {
+                std::string category = it->name.GetString();
+                int categoryIndex = this->categoryRange->getCategoryNumber(category);
+                WordCountList *l = this->getWordCountList(timeSlotIndex, categoryIndex);
+
+                const json::Value &value = it->value;
+                json::Value::ConstMemberIterator wordIt = value.MemberBegin();
+                l->reserve(value.Size());
+                while (wordIt != value.MemberEnd()) {
+                    std::string word = it->name.GetString();
+                    int count = it->value.GetInt();
+                    int wordIndex = this->dictionary->get(word);
+                    l->put(wordIndex, count);
+                    ++it;
+                }
+            }
+            current = utils::nextDate(current);
+        }
+    }
+
+};
+
+#endif /* __BASIC_DATA_MODEL_H__ */
---
+++ pcta/pcta/DataModel.h
@@ -0,0 +1,54 @@
+#ifndef __DATA_MODEL_H__
+#define __DATA_MODEL_H__
+
+#include <vector>
+
+#include "../model/Dictionary.h"
+#include "../model/TimeRange.h"
+#include "../model/CategoryRange.h"
+#include "../model/WordCount.h"
+#include "../model/WordCountList.h"
+
+class DataModel {
+
+protected:
+    Dictionary *dictionary;
+    TimeRange *timeRange;
+    CategoryRange *categoryRange;
+    int timeSlotNumber;
+    int categoryNumber;
+
+public:
+    DataModel() {
+        this->dictionary = NULL;
+        this->timeRange = NULL;
+        this->categoryRange = NULL;
+        this->timeSlotNumber = 0;
+        this->categoryNumber = 0;
+    }
+
+    DataModel(Dictionary *dictionary, TimeRange *timeRange, CategoryRange *categoryRange) {
+        this->dictionary = dictionary;
+        this->timeRange = timeRange;
+        this->categoryRange = categoryRange;
+        this->timeSlotNumber = timeRange->getTimeSlotNumber();
+        this->categoryNumber = categoryRange->getCategoryNumber();
+    }
+
+    virtual ~DataModel() {}
+
+    TimeRange *getTimeRange() {
+        return this->timeRange;
+    }
+
+    CategoryRange *getCategoryRange() {
+        return this->categoryRange;
+    }
+
+    virtual WordCountList *getWordCountList(int m, int n) = 0;
+
+    virtual std::vector<WordCountList *>
+    getListsByRange(int timeStart, int timeEnd, int categoryStart, int categoryEnd) = 0;
+};
+
+#endif /* __DATA_MODEL_H__ */
